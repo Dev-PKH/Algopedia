c22f40db9167ae5251100b9f7796e594b87fb2fd
Dev-PKH <tapasse12@gmail.com>
2025-05-26 20:18:26 +0900

    [level 2] Title: [3차] n진수 게임, Time: 12.59 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const string hexa = "0123456789ABCDEF";
+
+string hexing(int idx, int n)
+{
+    if(idx < n) return string("") + hexa[idx%n];
+    return hexing(idx/n, n) + hexa[idx%n];
+}
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    string result = "";
+    
+    for(int i=0; i<t*m; i++)
+        result += hexing(i, n);
+    
+    int cnt = 0;
+    for(auto c : result)
+    {
+        cnt++;
+        if(cnt == p && t > 0) {answer += c; t--;}
+        if(cnt == m) cnt = 0;
+    }
+    
+    return answer;
+}
