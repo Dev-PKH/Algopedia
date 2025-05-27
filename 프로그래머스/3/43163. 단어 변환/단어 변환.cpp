2263d59f7743f96708981e780e6807707aa9586b
Dev-PKH <tapasse12@gmail.com>
2025-05-27 11:28:59 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -2,20 +2,14 @@
 
 using namespace std;
 
-vector<string> s;
+
 bool vis[52];
 int result = 0x7fffffff;
 int row = 0; // word 단어 개수
 int col = 0; // word의 길이
 
-void DFS(string b, string& t, int idx, int cnt)
+void DFS(vector<string>& s, string b, string& t, int idx, int cnt)
 {
-    if(idx == row)
-    {
-        if(t == b) result = min(result, cnt);
-        return;
-    }
-    
     if(t == b) 
     {
         result = min(result, cnt);
@@ -25,6 +19,7 @@ void DFS(string b, string& t, int idx, int cnt)
     for(int i=0; i<row; i++)
     {
         if(vis[i]) continue;
+        
         int temp = 1;
         for(int j=0; j<col; j++)
             if(b[j] != s[i][j]) temp--;
@@ -32,7 +27,7 @@ void DFS(string b, string& t, int idx, int cnt)
         if(temp >= 0)
         {
             vis[i] = 1;
-            DFS(s[i],t,idx+1, cnt+1);
+            DFS(s, s[i],t,idx+1, cnt+1);
             vis[i] = 0;
         }
     }
@@ -41,9 +36,8 @@ void DFS(string b, string& t, int idx, int cnt)
 int solution(string begin, string target, vector<string> words) {
     row = words.size();
     col = words[0].length();
-    s = words;
-    
-    DFS(begin, target, 0, 0);
+
+    DFS(words, begin, target, 0, 0);
     
     if(result == 0x7fffffff) result = 0;
     return result;
