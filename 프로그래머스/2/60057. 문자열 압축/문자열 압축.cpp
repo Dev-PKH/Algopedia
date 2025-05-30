06d2755a953fdfd9ed71dddaa351ccc6eecc7b2e
Dev-PKH <tapasse12@gmail.com>
2025-05-30 12:13:46 +0900

    [level 2] Title: 문자열 압축, Time: 0.60 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -5,29 +5,28 @@ using namespace std;
 int solution(string s) {
     int len = s.length();
     int answer = len;
-
+    
+    
     for(int i=1; i<=len/2; i++)
     {
         string str = "";
-        string cur = s.substr(0,i);
+        string temp = s.substr(0,i);
         int cnt = 1;
-        
-        for(int j=i; j<len; j+= i)
+        for(int j=i; j<len; j+=i)
         {
-            string temp = s.substr(j,i);
+            string cur = s.substr(j,i);
             if(temp.compare(cur) == 0) cnt++;
             else
             {
                 if(cnt > 1) str += to_string(cnt);
-                
-                str += cur;
+                str += temp;
+                temp = cur;
                 cnt = 1;
-                cur = temp;
             }
         }
         
         if(cnt > 1) str += to_string(cnt);
-        str += cur;
+        str += temp;
         answer = min(answer, (int)str.length());
     }
     
