d663f41af025b09681e297d837a2270dfebb11df
Dev-PKH <tapasse12@gmail.com>
2025-05-30 10:34:50 +0900

    [level 2] Title: 전화번호 목록, Time: 490.39 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 bool solution(vector<string> pb) {
     bool answer = true;
+    unordered_map<string, int> s;
     
-    sort(pb.begin(), pb.end());
+    for(auto c : pb)
+        s[c] = 1;
     
-    for(int i=0; i<pb.size()-1; i++)
-        if(pb[i].compare(pb[i+1].substr(0, pb[i].length())) == 0)
-            return false;
-
+    for(int i=0; i<pb.size();i++)
+    {
+        string str = "";
+        for(int j=0; j<pb[i].length()-1; j++)
+        {
+            str += pb[i][j];
+            if(s[str])
+            {
+                return false;
+            }
+        }
+    }
     
     return answer;
 }
