a1b27d17ee9c960f6589937d62049e05dc8fc520
Dev-PKH <tapasse12@gmail.com>
2025-08-04 01:28:32 +0900

    [Silver I] Title: 선물할인, Time: 20 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/25947. 선물할인/선물할인.cc
+++ 백준/Silver/25947. 선물할인/선물할인.cc
@@ -13,27 +13,36 @@ int main() {
     cin.tie(0);
 
     cin >> n >> b >> a;
-    for (int i = 1; i <= n; i++) cin >> arr[i];
+    for (int i=1; i<=n; i++) cin >> arr[i];
 
-    sort(arr + 1, arr + n + 1);
+	sort(arr+1, arr+n+1);
+	
+	for(int i=1; i<=n; i++)
+	{
+		sum[i] = sum[i-1] + arr[i];
+		hsum[i] = hsum[i-1] + arr[i]/2;
+	}
 
-    for (int i = 1; i <= n; i++) {
-        sum[i] = sum[i - 1] + arr[i];             // 정가 누적합
-        hsum[i] = hsum[i - 1] + arr[i] / 2;       // 할인가 누적합
-    }
-
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        // i개의 선물을 살 때
-        if (sum[i] <= b) ans = i;
-        else {
-            int k = min(a, i); // 할인 가능한 개수
-            // i개 중 뒤에서 k개 할인 적용
-            ll cost = sum[i] - ( (sum[i] - sum[i - k]) - (hsum[i] - hsum[i - k]) );
-            if (cost <= b) ans = i;
-        }
-    }
-
-    cout << ans;
-    return 0;
+	int cnt = 0;
+	for(int i=n; i>0; i--)
+	{
+		if(sum[i]<=b)
+		{
+			cnt = i;
+			break;
+		}
+		else
+		{
+			int k = min(i, a);
+			ll num = sum[i] - ((sum[i]-sum[i-k]) - (hsum[i] - hsum[i-k]));
+			
+			if(num <= b)
+			{
+				cnt = i;
+				break;
+			}
+		}
+	}
+	
+	cout << cnt;
 }
