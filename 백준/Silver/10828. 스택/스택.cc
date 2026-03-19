3a692228a325c7aa64d32958d1228be93bed5e8b
Dev-PKH <tapasse12@gmail.com>
2026-03-19 23:31:09 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,48 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+stack<int> st;
+int n;
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-	stack<int> st;
-	int num;
-	string s;
-	int value;
-	cin >> num;
+	cin >> n;
 	
-	while(num--)
+	string s;
+	int num;
+	
+	while(n--)
 	{
 		cin >> s;
-		
-		if(s.compare("push") == 0)
+		if(s == "push")
 		{
-			cin >> value;
-			st.push(value);
+			cin >> num;
+			st.push(num);
 		}
-		else if(s.compare("pop") == 0)
+		else if(s == "pop")
 		{
 			if(st.empty()) cout << -1 << '\n';
-			else{
+			else
+			{
 				cout << st.top() << '\n';
-			st.pop();
+				st.pop();
 			}
 		}
-		else if(s.compare("size") ==0)
+		else if(s == "top")
+		{
+			if(st.empty()) cout << -1 << '\n';
+			else cout << st.top() << '\n';
+		}
+		else if(s == "size")
 		{
 			cout << st.size() << '\n';
 		}
-		else if(s.compare("empty") == 0)
+		else
 		{
 			if(st.empty()) cout << 1 << '\n';
 			else cout << 0 << '\n';
 		}
-		else
-		{
-			if(st.empty()) cout << -1 <<'\n';
-			else cout << st.top() << '\n';
-		}
 	}
+	
 	
 	return 0;
 }
