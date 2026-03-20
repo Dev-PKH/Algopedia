7a196271a13c0a9a815c8f20f06d8dac694561d0
Dev-PKH <tapasse12@gmail.com>
2026-03-20 09:23:48 +0900

    [Silver IV] Title: 큐, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,45 +1,54 @@
-#include <iostream>
-#include <queue>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+queue<int> q;
+int n;
+
 int main() {
+	ios::sync_with_stdio();
+	cin.tie(0);
 	
-	ios_base :: sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin >> n;
 	
+	string s;
 	int num;
-	string str;
-	queue<int> que;
-	cin >> num;
 	
-	for(int i=0; i<num; i++){
-		cin >> str;
-		if(str == "push"){
-			int value;
-			cin >> value;
-			que.push(value);
+	while(n--)
+	{
+		cin >> s;
+		if(s == "push")
+		{
+			cin >> num;
+			q.push(num);
 		}
-		else if(str == "pop"){
-			if(!que.empty()){
-				cout << que.front() << '\n';
-				que.pop();
+		else if(s == "pop")
+		{
+			if(q.empty()) cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
 			}
-			else cout << "-1\n";
 		}
-		else if(str == "size") cout << que.size() << '\n';
-		else if(str == "empty") cout << que.empty() << '\n';
-		else if(str == "front"){
-			if(!que.empty()) cout << que.front() << '\n';
-			else cout << "-1\n";
+		else if(s == "size")
+		{
+			cout << q.size() << '\n';
 		}
-		else if(str == "back"){
-			if(!que.empty()) cout << que.back() << '\n';
-			else cout << "-1\n";
+		else if(s == "empty")
+		{
+			if(q.empty()) cout << 1 << '\n';
+			else cout << 0 << '\n';
 		}
-		
+		else if(s == "front")
+		{
+			if(q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+		else
+		{
+			if(q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
 	}
 	
 	return 0;
