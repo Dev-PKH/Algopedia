c78e97f6e9623f68a19794c67a3a07cae278cdda
Dev-PKH <tapasse12@gmail.com>
2026-03-22 11:02:45 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -4,44 +4,42 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	string s = "test";
 	
 	while(1)
 	{
+		string s;
 		stack<char> st;
+		
 		getline(cin, s);
 		
-		if(s.compare(".") == 0) break;
+		if(s[0] == '.') break;
 		
-		bool check = true;
-		
-		for(auto i : s)
+		for(auto i:s)
 		{
-			if(i == '(' || i == '[') st.push(i);
+			if(i=='(' || i=='[') st.push(i);
 			else if(i == ')')
 			{
-				if(st.empty() || st.top() != '(') {
-					check = false;
-					break;
+				if(st.empty() || st.top() != '(') 
+				{
+					st.push(1);
+					break;	
 				}
 				else st.pop();
 			}
 			else if(i == ']')
 			{
-				if(st.empty() || st.top() != '[') { 
-					check = false;
-					break;
+				if(st.empty() || st.top() != '[') 
+				{
+					st.push(1);
+					break;	
 				}
 				else st.pop();
 			}
 		}
 		
-		if(!st.empty()) check = false;
-		
-		if(check) cout << "yes" << '\n';
+		if(st.empty()) cout << "yes" << '\n';
 		else cout << "no" << '\n';
-	}
+ 	}
 	
 	return 0;
 }
