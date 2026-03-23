24b59bce5bdf76bcab83632b03be38ed320653d7
Dev-PKH <tapasse12@gmail.com>
2026-03-23 12:42:50 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -3,52 +3,64 @@ using namespace std;
 
 #define X first
 #define Y second
+
 int board[502][502];
 bool vis[502][502];
-int n,m;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
+int n, m, cnt=0, result=0;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
 	cin >> n >> m;
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++) cin >> board[i][j];
 	
-	queue<pair<int,int>> Q;
-	int num=0;
-	int max=0;
-
+	queue<pair<int, int>> q;
+	
+	
 	for(int i=0; i<n; i++)
 	{
 		for(int j=0; j<m; j++)
 		{
-			if(vis[i][j] || board[i][j] == 0) continue;
-			Q.push({i,j});
-			vis[i][j] = 1;
-			num++;
-			int cnt =1;
-			
-			while(!Q.empty())
-			{
-				pair<int,int> cur = Q.front(); Q.pop();
-				for(int k=0; k<4; k++)
-				{
-					int nx = cur.X + dx[k], ny = cur.Y + dy[k];
-					if(nx <0 || ny <0 || nx >= n || ny >=m) continue;
-					if(vis[nx][ny] || board[nx][ny] == 0) continue;
-					Q.push({nx,ny});
-					vis[nx][ny] = true;
-					cnt++;
-				}
-			}
-			if(cnt > max) max = cnt;
+			cin >> board[i][j];
 		}
 	}
-	cout << num << '\n' << max;
+	
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<m; j++)
+		{
+			if(!vis[i][j] && board[i][j] == 1)
+			{
+				vis[i][j] = 1;
+				q.push({i,j});
+				int size = 0;
+				cnt++;
+				
+				while(!q.empty())
+				{
+					auto cur = q.front(); q.pop();
+					size++;
+					
+					for(int idx=0; idx<4; idx++)
+					{
+						int nx = cur.X + dx[idx], ny = cur.Y + dy[idx];
+						if(nx<0 || ny<0 || nx >=n || ny >= m) continue;
+						if(vis[nx][ny] || board[nx][ny]==0) continue;
+						q.push({nx,ny});
+						vis[nx][ny] = 1;
+					}
+					
+				}
+				
+				if(size > result) result = size;
+			}
+		}
+	}
+	
+	cout << cnt << '\n' << result;
 	
 	return 0;
 }
